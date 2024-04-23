4c578c996114d1cf2720b46fc20c1385cef7a902
PopoDev <popocompte@gmail.com>
2024-04-23 09:05:03 -0700

    Add speedup visualization

--- run_translation_t5bild_unaligned.sh
+++ run_translation_t5bild_unaligned.sh
@@ -4,7 +4,7 @@ python src/run_translation.py \
 --tokenizer_name google/mt5-small \
 --dataset_name iwslt2017 --dataset_config_name iwslt2017-de-en \
 --source_lang de --target_lang en \
---fallback_threshold 0.6 --rollback_threshold 5 \
+--fallback_threshold 0.8 --rollback_threshold 0.5 \
 --metric_for_best_model bleu \
 --num_beam 1 \
 --evaluation_strategy epoch --save_strategy epoch \
