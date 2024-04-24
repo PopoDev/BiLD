003bc5ce8dba458675bceb08d2f6d064c7562549
PopoDev <popocompte@gmail.com>
2024-04-23 17:07:30 -0700

    Add distributed computation grid search

--- src/grid_search.py
+++ src/grid_search.py
@@ -1,7 +1,7 @@
 import subprocess
 import argparse
 
-FB_THRESHOLDS = [0.2, 0.35, 0.5, 0.65, 0.8]
+FB_THRESHOLDS = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 RB_THRESHOLDS = [0.5, 0.75, 1, 1.25, 1.5]
 
 if __name__ == "__main__":
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true", default=False, help="Debug mode.")
     parser.add_argument("--experiment", type=str, default="iwslt2017", choices=["iwslt2017", "wmt2014", "xsum", "cnndm"], help="Experiment to run.")
     parser.add_argument("--aligned", action="store_true", default=False, help="Use aligned data.")
+    parser.add_argument("--gpu", type=int, help="GPU to use.")
     args = parser.parse_args()
 
     command = []
 
     if args.experiment == "iwslt2017":
-        command.append(f"./run_iwslt2017_{args.type}.sh")
+        command.append(f"./run_iwslt2017_{"aligned" if args.aligned else "unaligned"}.sh")
     elif args.experiment == "wmt2014":
         raise NotImplementedError("wmt2014 is not implemented yet.")
     elif args.experiment == "xsum":
@@ -23,8 +24,13 @@ if __name__ == "__main__":
         raise NotImplementedError("cnndm is not implemented yet.")
     else:
         raise ValueError(f"Unknown experiment: {args.experiment}")
-
-    for fb_threshold in FB_THRESHOLDS:
+    
+    if args.gpu == 0:
+        fb_thresholds = FB_THRESHOLDS[:len(FB_THRESHOLDS) // 2]
+    elif args.gpu == 1:
+        fb_thresholds = FB_THRESHOLDS[len(FB_THRESHOLDS) // 2:]
+    
+    for fb_threshold in fb_thresholds:
         for rb_threshold in RB_THRESHOLDS:
             command.append(str(fb_threshold))
             command.append(str(rb_threshold)) 
