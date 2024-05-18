d775a91c5bb06d825449be5e429229b2097d148d
Seulchan Han <paulh27@uw.edu>
2024-05-17 22:15:31 -0700

    merge

---
+++ run_iwslt_hp_search.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+python src/hyperparameter_search.py \
+--experiment iwslt2017 \
+--fb_thresholds 0.5 0.6 0.7 0.8 0.9 \
+--rb_thresholds 1 2 3 4 5 6 7 8 9 10 \
+--output_dir /local1/hfs/CSE481N_Project/out/iwslt_hp_search \
+--model_name_large kssteven/mT5-large-iwslt2017-de-en \
+--model_name_small kssteven/mT5-small-iwslt2017-de-en-bild-aligned \
+--max_eval_samples 888 \
+--tokenizer_name google/mt5-small \
+--dataset_name iwslt2017 \
+--dataset_config_name iwslt2017-de-en 
--- src/hyperparameter_search.py
+++ src/hyperparameter_search.py
@@ -7,20 +7,20 @@ FB_THRESHOLDS = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
 RB_THRESHOLDS = [1, 2, 3, 4, 5]
 
 command_template = Template(
-"""python src/run_summarization.py 
+"""python $source_file_path
 --output_dir $output_dir
+--cache_dir /tmp/cache
 --model_name_large $model_name_large 
 --model_name_small $model_name_small
 --tokenizer_name $tokenizer_name
 --dataset_name $dataset_name 
---source_prefix $source_prefix
+--dataset_config_name $dataset_config_name 
 --fallback_threshold $fallback_threshold 
 --rollback_threshold $rollback_threshold 
 --num_beam 1 
 --evaluation_strategy epoch --save_strategy epoch 
 --do_eval 
 --per_device_eval_batch_size 1 
---trust_remote_code True 
 --predict_with_generate
 --max_eval_samples $max_eval_samples"""
 )
@@ -40,18 +40,25 @@ if __name__ == "__main__":
     parser.add_argument("--max_eval_samples", type=str, default='', help="Number of samples to use in eval.")
     parser.add_argument("--tokenizer_name", type=str, default='', help="Tokenizer name.")
     parser.add_argument("--dataset_name", type=str, default='', help="Dataset name.")
+    parser.add_argument("--dataset_config_name", type=str, default='', help="Dataset name.")
 
     args = parser.parse_args()
 
     for fb_threshold in args.fb_thresholds:
         for rb_threshold in args.rb_thresholds:
+            if args.experiment == 'iwslt2017' or args.experiment == 'wmt2014':
+                source_file_path = 'src/run_translation.py'
+            else:
+                source_file_path = 'src/run_summarization.py'
+
             command = command_template.substitute({
+                'source_file_path': source_file_path,
                 'output_dir': f"{args.output_dir}/{args.experiment}/fb={fb_threshold}_rb={rb_threshold}",
                 'model_name_large': args.model_name_large,
                 'model_name_small': args.model_name_small,
                 'tokenizer_name': args.tokenizer_name,
                 'dataset_name': args.dataset_name,
-                'source_prefix': args.source_prefix,
+                'dataset_config_name': args.dataset_config_name,
                 'fallback_threshold': fb_threshold,
                 'rollback_threshold': rb_threshold,
                 'max_eval_samples': args.max_eval_samples
