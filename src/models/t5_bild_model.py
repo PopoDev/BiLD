f27fe2ce44d050a16559624d752aacb3a5438322
PopoDev <popocompte@gmail.com>
2024-05-26 14:36:55 -0700

    Fix transformers update _prepare_decoder_input_ids_for_generation() got an unexpected keyword argument 'bos_token_id'

--- src/models/t5_bild_model.py
+++ src/models/t5_bild_model.py
@@ -526,7 +526,6 @@ class T5BiLDModel(nn.Module, GenerationMixin):
             model_input_name=model_input_name,
             model_kwargs=model_kwargs,
             decoder_start_token_id=self.generation_config.decoder_start_token_id,
-            bos_token_id=self.generation_config.bos_token_id,
             device=inputs_tensor.device,
         )
 
