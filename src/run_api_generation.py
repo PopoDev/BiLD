4847b023e8327c80da9db6f5f31af85d95380a3a
PopoDev <popocompte@gmail.com>
2024-05-29 09:31:53 -0700

    Remove unused API Gen pipeline

--- src/run_api_generation.py
+++
@@ -1,9 +0,0 @@
-from experiments.api_generation.run import run_summarization
-from experiments.api_generation.arguments import ModelArguments, DataTrainingArguments
-from transformers import HfArgumentParser, Seq2SeqTrainingArguments
-
-if __name__ == "__main__":
-    parser = HfArgumentParser(
-        (ModelArguments, DataTrainingArguments, Seq2SeqTrainingArguments)
-    )
-    run_summarization(parser)
